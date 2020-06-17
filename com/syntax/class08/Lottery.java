838b6e241bb84e016128ca6adaf786f6603dfdb5
SlimemobX <ferrufino2@gmail.com>
2020-06-16 21:37:19 -0400

    update

---
+++ com/syntax/class08/Lottery.java
@@ -0,0 +1,34 @@
+package com.syntax.class08;
+
+import java.util.Scanner;
+
+public class Lottery {
+	public static void main(String[] args) {
+	// We are playing a lottery game and the lucky number is 17; 
+	// We want to keep asking the user to enter any numbers from 1-20; 
+	// until he guesses the lucky number --> In that case we print "Congrats, you won!"
+	
+	// if I use while, I have to ask two times
+		Scanner scan = new Scanner(System.in);
+		int lucky = 17;
+		int num;
+	
+//		while (num != lucky) {
+//			System.out.println("please enter a number 1-20: ");
+//			num = scan.nextInt();
+//		}
+			
+		
+	// if I use do while loop, I have to ask one time
+	do {
+		System.out.println("please enter a number 1-20: ");
+		num = scan.nextInt();	
+	    }while (num != lucky);
+		
+	System.out.println("Congrats, you won!");
+	
+	
+	
+	
+	}
+}
