21fac8b4e3824d1cc64537175db989412aac4811
SlimemobX <ferrufino2@gmail.com>
2020-06-30 20:24:22 -0400

    user-defined methods

---
+++ com/syntax/class16/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.syntax.class16;
+
+public class CalculatorTest {
+	public static void main(String[] args) {
+		
+		// call method add();
+		
+		Calculator c = new Calculator();
+		c.add(100,200);
+		c.add(10,20);
+		c.add(1000,2000);
+		
+		c.sub(100, 10);
+		
+		c.div(10, 2);
+		
+		c.mult(10, 10);
+		
+	}
+}
