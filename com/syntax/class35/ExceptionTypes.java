07d4434035de48ea0c8299b87715780b920376b4
SlimemobX <ferrufino2@gmail.com>
2020-08-22 11:32:12 -0400

    Recap And Exception

---
+++ com/syntax/class35/ExceptionTypes.java
@@ -0,0 +1,17 @@
+package com.syntax.class35;
+
+import java.io.FileInputStream;
+
+public class ExceptionTypes {
+	
+	public static void main(String[] args) {
+		
+		// Thread.sleep(2000);
+		
+		String xlPath = System.getProperty("user.dir") + "/testdata/SampleTestData.xlsx";
+		FileInputStream fis = new FileInputStream(xlPath);
+		
+		
+	}
+
+}
---
+++ com/syntax/class35/Recap.java
@@ -0,0 +1,55 @@
+package com.syntax.class35;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Recap {
+	
+	public static void main(String[] args) throws IOException {
+		
+		String xlPath = System.getProperty("user.dir") + "/testdata/SampleTestData.xlsx";
+
+		FileInputStream fis = new FileInputStream(xlPath);
+
+		// create an object of Workbook
+		Workbook book = new XSSFWorkbook(fis);
+		Sheet sheet = book.getSheet("Sheet1");
+
+		// get numbers of rows and columns
+		int rows = sheet.getPhysicalNumberOfRows();
+
+		int col = sheet.getRow(0).getPhysicalNumberOfCells();
+		
+		// create empty list
+		List<Map<String, String>> xlList = new ArrayList<>();
+
+		// loop over rows
+		for (int r = 1; r < rows; r++) {
+			// for every row create a new map
+			Map<String, String> map = new LinkedHashMap<>();
+			// loop over every column
+			for (int c = 0; c < col; c++) {
+				// get keys from 1 row (header)
+				String key = sheet.getRow(0).getCell(c).toString();
+				// get values from other rows
+				String value = sheet.getRow(r).getCell(c).toString();
+
+				map.put(key, value);
+
+			}
+			// add created map with values into List
+			xlList.add(map);
+		}
+		
+	}
+
+}
